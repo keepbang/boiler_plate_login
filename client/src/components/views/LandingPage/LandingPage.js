53a9a11bae543b88a6798d4f8c5fb5e558d97a64
keepbang <rlqud1125@naver.com>
2020-07-14 00:02:25 +0900

    react component create

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <div>
+            LandingPage 페이지
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+    return (
+        <div>
+            LoginPage 페이지
+        </div>
+    )
+}
+
+export default LoginPage
