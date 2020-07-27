e5fc8c6accd8b54f3b83371d372b5c47ee8cd204
keepbang <rlqud1125@naver.com>
2020-07-27 17:57:00 +0900

    logout 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,13 +2,23 @@ import React,{useEffect} from 'react'
 import axios from 'axios';
 
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response))
     }, [])
 
+    const onClickHandler = () => {
+        axios.get(`/api/users/logout`)
+        .then(res => {
+            if(res.data.success){
+                props.history.push('/login')
+            }else{
+                alert('Fail Logout')
+            }
+        })
+    }
 
     return (
         <div style={{
@@ -16,6 +26,9 @@ function LandingPage() {
             width:'100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
